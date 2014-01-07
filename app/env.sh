12b79552e92cfe4189071f4f1f7572f0c3340527
Colin Surprenant <colin.surprenant@gmail.com>
2014-01-07 00:14:35 -0500

    updates

--- app/env.sh
+++ app/env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-REDIS_ID=$(docker ps --notrunc=true | grep "devopsmtl/redis" |awk '{print $1}')
+REDIS_ID=$(docker ps --notrunc=true | grep "devopsmtl/redis" | awk '{print $1}')
 export REDIS_HOST=$(docker inspect $REDIS_ID | grep IPAddress | awk -F\" '{print $4}')
 echo $REDIS_HOST
